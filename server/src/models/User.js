83a1a91f7d82f1720eeb3fb09aca5e3c6077230a
ZehaanArshad <zehaanarshad@gmail.com>
2026-09-12 15:24:10 +0530

    feat(user): implement password hashing and comparison methods using bcrypt

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema(
   {
@@ -40,6 +41,16 @@ userSchema.set('toJSON', {
     return ret;
   },
 });
+
+userSchema.pre('save', async function hashPassword() {
+  if (!this.isModified('password')) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = function comparePassword(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
